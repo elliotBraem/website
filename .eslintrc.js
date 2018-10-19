42014efdb8ac6b61fa479967f11974e827cb918b
elliotBraem <braemelliot@gmail.com>
2018-10-18 19:01:39 -0500

    chore: resets to a vue website

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  extends: "google",
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
