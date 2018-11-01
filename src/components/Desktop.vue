cef471d0879056aa2ff3df9d3eaf9d80bf0ccfdb
elliotBraem <braemelliot@gmail.com>
2018-10-31 22:14:23 -0500

    feat: replaces logo, orients content in center of page

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   .wrapper
     .background
-      .center-profile
-        img(src="./assets/logo.svg" alt="")
-        h2 Hi, I'm an elevator pitch
-    .foreground
       .link-layer
-        a projects
-        a contact
+        #projects.position-absolute
+          a projects
+        #contact.position-absolute
+          a contact
+      .foreground
+        .center-profile.text-center
+          img(src="../assets/logo.svg" alt="")
+          h2 This is the logo
+          h2 Hi, I'm an elevator pitch
+
 </template>
 
 <script>
@@ -20,4 +24,28 @@ export default {
   .wrapper
     width = 100%
     height = 100vh
+
+  .background
+  .foreground
+    margin 0 auto
+
+  .center-profile
+  .link-layer
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+
+  .link-layer
+    min-width: 60%
+    min-height: 60%
+
+  #projects
+    left: 0
+    top: 15%
+
+  #contact
+    right: 0
+    bottom: 15%
+
 </style>
