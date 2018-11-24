45b180792910606320fb7b7183f9cd643a4ae7f7
elliotBraem <braemelliot@gmail.com>
2018-11-23 19:30:34 -0600

    chore: remove unneccessaries

---
+++ pages/projects/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-html="content.default"/>
+</template>
+
+
+<script>
+export default {
+  async asyncData({ params }) {
+    try {
+      const fileContent = await import(`~/assets/projects/${params.slug}/${
+        params.slug
+      }.md`)
+      return {
+        content: fileContent
+      }
+    } catch (e) {
+      console.log('Error caught: ' + e)
+    }
+  }
+}
+</script>
