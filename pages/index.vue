408fa20b2e64e469a1528d40e27e404f272e3a51
elliotBraem <braemelliot@gmail.com>
2018-11-23 19:43:40 -0600

    feat: beginning conversion from former website

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,70 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        personal-website
-      </h1>
-      <h2 class="subtitle">
-        Elliot Braem&#39;s personal website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+  .wrapper
+    .background
+      .link-layer
+        #projects.position-absolute
+          h4
+            router-link(to="/projects") projects
+        #contact.position-absolute
+          h4
+            a.d-block(href="#") contact
+            span.d-inline-block
+              i.fab.fa-github
+              i.fab.fa-linkedin
+              i.fab.fa-instagram
+      .foreground
+        .center-profile.text-center
+          img(src="../assets/logo.svg" alt="")
+          h2 Hi, my name is Elliot Braem and I am a currently
+            |  writing an Elevator Pitch to put here
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
-}
+};
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="stylus" scoped>
+  .wrapper
+    width: 100%
+    height: 100vh
+  .background
+  .foreground
+    margin 0 auto
+  .center-profile
+  .link-layer
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  .center-profile
+    max-width: 50%
+    img
+      max-height: 20vmin;
+    h2
+      font-size: 3vh
+  .link-layer
+    width: 100%
+    height: 100vh
+  #projects
+    width: 30%
+    position: absolute
+    left: 20%
+    top: 15%
+    transform: translate(-20%,-15%)
+  #contact
+    width: 30%
+    position: absolute
+    left: 70%
+    top: 80%
+    transform: translate(-85%,-70)
+  @media screen and (min-width: 426px)
+    #projects
+      left: 25%
+      top: 25%
+      transform: translate(-25%,-25%)
+    #contact
+      left: 75%
+      top: 75%
+      transform: translate(-75%,-75)
 </style>
