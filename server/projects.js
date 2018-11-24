8e42c2765fd25d6413e6960fd26a35243b6b2ccf
elliotBraem <braemelliot@gmail.com>
2018-11-24 00:59:03 -0600

    refactor: sets project directories functions to own js file in /server

---
+++ server/projects.js
@@ -0,0 +1,35 @@
+const { lstatSync, readdirSync } = require('fs')
+const { join } = require('path')
+
+/**
+ * Checks if the source is a directory
+ * @param {string}: source - file/folder to check
+ * @return {boolean} True if directory, else false
+ */
+const isDirectory = source => lstatSync(source).isDirectory()
+
+/**
+ * Return the directories in .../'source'
+ * @param {string}: source - folder to check
+ * @return {array} The directories in .../'source'
+ */
+const directories = source =>
+  readdirSync(source) // Read this directory
+    .map(name => join(source, name)) // For all contents, create path 'source/name'
+    .filter(isDirectory) // Check that content is a directory
+
+/**
+ * Return the files of type .md in source
+ * @param {string}: source - file to check
+ * @return {boolean} True if of type .md, else false
+ */
+const getMarkdown = source => source.filter(file => file.match(/.*\.(md)/gi))
+
+/**
+ * Returns the slug for the project
+ * @param {string}: post - name of directory
+ * @return {string} slug path
+ */
+const getSlugs = name => '/projects/${' + name + '}'
+
+module.exports = { directories, getSlugs }
--- store/index.js
+++ store/index.js
@@ -1,23 +1,5 @@
 import Vuex from 'vuex'
-const { lstatSync, readdirSync } = require('fs')
-const { join } = require('path')
-
-/**
- * Checks if the source is a directory
- * @param {string}: source - file/folder to check
- * @return {boolean} True if directory, else false
- */
-const isDirectory = source => lstatSync(source).isDirectory()
-
-/**
- * Return the directories in .../'source'
- * @param {string}: source - folder to check
- * @return {array} The directories in .../'source'
- */
-const directories = source =>
-  readdirSync(source) // Read this directory
-    .map(name => join(source, name)) // For all contents, create path 'source/name'
-    .filter(isDirectory) // Check that content is a directory
+const { directories } = require('~/server/projects')
 
 const createStore = () => {
   return new Vuex.Store({
