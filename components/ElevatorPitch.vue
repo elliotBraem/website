8048e97da2a102284a7e26f965df4c5408dad0bb
elliotBraem <braemelliot@gmail.com>
2018-11-30 14:38:40 -0600

    fix: grammar mistake

--- components/ElevatorPitch.vue
+++ components/ElevatorPitch.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div.mt-3
-    h2 Hi! I'm Elliot.<br> I have a passion for creating software to solve problems, <br> as well as a growing interest and understanding of how A.I. can be used to solve them. 
+    h2 Hi! I'm Elliot.<br> I have a passion for creating software to solve problems, <br> as well as a growing interest in and understanding of how A.I. can be used to solve them. 
 </template>
 
 <style lang="stylus" scoped>
