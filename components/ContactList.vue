670f9fc0cca0da03357545c0b16eaca66a876977
Elliot Braem <elliot@brae.me>
2020-05-21 12:40:20 -0500

    feat: removes facebook icon and updates twitter

--- components/ContactList.vue
+++ components/ContactList.vue
@@ -2,9 +2,8 @@
   .d-inline-block
     Icon.mr-2(:href="'https://github.com/elliotBraem'" :type="'github'")
     Icon.mr-2(:href="'https://www.linkedin.com/in/elliot-braem-91165a112/'" :type="'linkedin'")
-    Icon.mr-2(:href="'https://www.facebook.com/profile.php?id=100001352883257'" :type="'facebook'")
     Icon.mr-2(:href="'https://www.instagram.com/efizzysnaps/'" :type="'instagram'")
-    Icon.mr-2(:href="'https://twitter.com/EFizzySnaps'" :type="'twitter'")
+    Icon.mr-2(:href="'https://twitter.com/efizzysnaps'" :type="'twitter'")
 </template>
 
 <script>
