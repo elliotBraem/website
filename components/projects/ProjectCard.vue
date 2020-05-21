6bdd516f75125a92bcb3a94ba88b36b75239318a
Elliot Braem <elliot@brae.me>
2020-05-21 12:19:20 -0500

    Widens project card and sheds weight

--- components/projects/ProjectCard.vue
+++ components/projects/ProjectCard.vue
@@ -1,15 +1,21 @@
 <template lang="pug">
-  b-container.fluid
-    div.project-card(v-b-toggle="'project-details-' + project.attributes.id" :style="BGcolor")
-      b-img.project-image(v-bind:src="project.attributes.img" fluid).mx-auto
-      h3.title.pl-3 {{project.attributes.title}}
-      b-collapse(v-bind:id="'project-details-' + project.attributes.id")
-        div.content.pl-3.pr-3
-          h6.description {{project.attributes.description}}
-          h4
-            a.github(:href="project.attributes.url" target="_blank")
-              font-awesome-icon.mr-2(:icon="['fab', 'github']")
-          //- div(v-html="project.html")
+  b-card.project-card(
+    v-b-toggle="'project-details-' + project.attributes.id"
+    :style="BGcolor"
+    footer-tag="footer"
+    footer-bg-variant="transparent"
+  )
+    b-img.project-image(v-bind:src="project.attributes.img")
+    b-collapse(v-bind:id="'project-details-' + project.attributes.id")
+      h6.description {{project.attributes.description}}
+    footer
+      b-row
+        b-col
+          h3 {{project.attributes.title}}
+        b-col
+          h3.github
+            a(:href="project.attributes.url" target="_blank")
+              font-awesome-icon(:icon="['fab', 'github']")
 </template>
 
 <script>
@@ -30,71 +36,74 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-  .project-card
-    position: relative
-    max-width:400px
-    border-radius: 5px
+<style scoped>
+.card-body {
+  padding: 0.25rem 1rem;
+}
 
-  .title
-    position: absolute
-    bottom: 0
+footer {
+  border: none;
+}
 
-  .description
-    font-weight: lighter
+.project-card {
+  width: 100%;
+  border: none;
+  margin: 20px 0;
+}
 
-  .project-image
-    width: 400px
-    height: 270px
+.project-image {
+  align-self: center;
+  max-height: 400px;
+}
 
-  .content
-    position: relative
-    padding-bottom: 2.5rem
+.github {
+  float: right;
+}
 
-  .github
-    position: absolute
-    bottom: 0.5rem
-    right: 0.5rem
+.description {
+  font-weight: lighter;
+}
 
-  /* Small Devices, Tablets */
-  @media only screen and (min-width : 768px)
-    .project-card
-      display: inline-block;
-      box-shadow: 0 1px 2px rgba(0,0,0,0.1);
-      -webkit-transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
-      transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
+/* Small Devices */
+/* @media only screen and (min-width: 600px) { */
+.project-card {
+  display: inline-block;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  -webkit-transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
+  transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
 
-    .project-card::after
-      content: ""
-      position: absolute
-      z-index: -1;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      box-shadow: 0 5px 15px rgba(0,0,0,0.3);
-      opacity: 0;
-      -webkit-transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
-      transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
+.project-card::after {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+  opacity: 0;
+  -webkit-transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
+  transition: all 1.2s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
 
-    .project-card:hover
-      -webkit-transform: scale(1.05, 1.05);
-      transform: scale(1.05, 1.05);
+.project-card:hover {
+  -webkit-transform: scale(1.05, 1.05);
+  transform: scale(1.05, 1.05);
+}
 
-    .project-card:hover::after
-      opacity: 1;
+.project-card:hover::after {
+  opacity: 1;
+}
 
-    .project-card:active
-      -webkit-transform: scale(1.0, 1.0);
-      transform: scale(1.0, 1.0);
-      transition: all .2s cubic-bezier(0.165, 0.84, 0.44, 1);
+.project-card:active {
+  -webkit-transform: scale(1, 1);
+  transform: scale(1, 1);
+  transition: all 0.2s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
 
-    .project-card:active::after
-      box-shadow: 0 5px 15px rgba(0,0,0,0.1);
-
-  /* Medium Devices, Desktops */
-  // @media only screen and (min-width : 992px)
-
-  /* Large Devices, Wide Screens */
-  // @media only screen and (min-width : 1200px)
+.project-card:active::after {
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+}
+/* } */
 </style>
